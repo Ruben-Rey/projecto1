5d73a051ea9006c80c8ca3e993aa750f332a02c9
Ruben-Rey <ibiza.reymundis@gmail.com>
2022-06-22 18:38:39 -0500

    Class Meta Departamento

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 class Departamento(models.Model):
-    name = models.CharField('Nombre', max_length=50, editable=False)
-    shor_name = models.CharField('Nombre Corto', max_length=20)
+    name = models.CharField('Nombre', max_length=50)
+    shor_name = models.CharField('Nombre Corto', max_length=20, unique=True)
     anulate = models.BooleanField('Anulado', default=False)
+
+    class Meta:
+        verbose_name = 'Mi Departamento'
+        verbose_name_plural = 'Areas de la empresa'
+        ordering = ['shor_name'] 
+
 
     def __str__(self):
         return self.shor_name
