54ffc8f950027844c540678c113fafc7503c0904
Ruben-Rey <ibiza.reymundis@gmail.com>
2022-06-21 23:58:51 -0500

    modelo Persona

--- applications/departamento/models.py
+++ applications/departamento/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Departamento(models.Model):
-    name = models.CharField('Nombre', max_length=50)
+    name = models.CharField('Nombre', max_length=50, editable=False)
     shor_name = models.CharField('Nombre Corto', max_length=20)
     anulate = models.BooleanField('Anulado', default=False)
 
     def __str__(self):
-        return str(self.id) + self.name + '-' + self.shor_name
+        return self.shor_name
--- applications/persona/models.py
+++ applications/persona/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from applications.departamento.models import Departamento
 
-# Create your models here.
+class Persona(models.Model):
+
+    JOB_CHOICES = (
+        ('0', 'CONTADOR'),
+        ('1', 'ADMINISTRADOR'),
+        ('2', 'ECONOMISTA'),
+        ('3', 'OTRO'),
+
+    )
+
+    first_name = models.CharField('Nombres', max_length=60)
+    last_name = models.CharField('apellidos', max_length=60)
+    job = models.CharField('Trabajo', max_length=1, choices=JOB_CHOICES)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
